4d8cce2781f30a84773d85eaa99d25e12df45c71
mzk7ue <mzk7ue@virginia.edu>
2026-01-15 20:11:53 +0000

    new changes

---
+++ day2.py
@@ -0,0 +1,21 @@
+# %%
+# Open the CodeSpace in your local VS Code
+# What is your terminal display "path"?
+# /workspaces/daytwostuff'
+
+# %%
+# Create a virtual environment
+# Should you include the environment in your repo or not? 
+# No, you should not include the virtual environment in your repo. 
+
+# Now, What is your terminal display "path"? Is it different?
+# /workspaces/daytwostuff
+# The terminal path is not different.
+
+# %% 
+# Load data
+import pandas as pd 
+df = pd.read_csv('tip.csv')
+print(df.head())
+
+# %%

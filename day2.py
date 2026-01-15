5b10da30ec05053e9433d8c5dceb11edb4f74a72
mzk7ue <mzk7ue@virginia.edu>
2026-01-15 20:13:16 +0000

    new changes

--- day2.py
+++ day2.py
@@ -1,10 +1,8 @@
-# %%
-# Open the CodeSpace in your local VS Code
+# %% Open the CodeSpace in your local VS Code
 # What is your terminal display "path"?
-# /workspaces/daytwostuff'
+# /workspaces/daytwostuff
 
-# %%
-# Create a virtual environment
+# %% Create a virtual environment
 # Should you include the environment in your repo or not? 
 # No, you should not include the virtual environment in your repo. 
 
@@ -12,10 +10,20 @@
 # /workspaces/daytwostuff
 # The terminal path is not different.
 
-# %% 
-# Load data
+# %% Viewing data
 import pandas as pd 
 df = pd.read_csv('tip.csv')
-print(df.head())
+df.head()
 
+# %% Extension Management 
+# What do you notice about the extension menu?
+# It shows me all the extensions I have installed locally and in the Codespace.
+
+# Review the capabilities, what are three useful elements of Data Wrangler?
+# The Data Wrangler extension automatically calculates the basic statistics (mean, median, standard deviation, etc.). It also showcases if there are any missing values in the dataset and visualizes the numerical variables. 
+
+# %% Package Managing 
+# Plotly version: 6.5.1
+# Why do we use a requirements.txt file?
+# It shows us all the package versions that were used when running this project, and allows others to do so as well on their own machines when cloning/forking the project. 
 # %%
